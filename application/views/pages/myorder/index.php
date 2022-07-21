9eca1d15926bb29cec8ba706dc04b657345ce7e1
ilham <t.20ti@mahasiswa.pcr.ac.id>
2022-07-21 22:08:12 +0700

    Initial commit

---
+++ application/views/pages/myorder/index.php
@@ -0,0 +1,48 @@
+<div style="background-color:#0C2D48;">
+<div class="container p-5">
+<div class="container">
+	<div class="row mt-4">
+		<div class="col">
+
+			<?php $this->load->view('layouts/_alert') ?>
+
+			<div class="card">
+				<h5 class="card-header text-center"><strong style="color : white">My Orders</strong></h5>
+				<div class="card-body">
+					<table class="table table-bordered text-center">
+						<thead>
+							<tr>
+								<th>Invoice</th>
+								<th>Date</th>
+								<th>Total</th>
+								<th>Status</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php foreach($orders as $o) : ?>
+								<tr>
+									<td><strong><a href="<?= base_url('myorder/detail/' . $o['invoice']) ?>"><?= $o['invoice'] ?></a></strong></td>
+									<td><?= $o['date'] ?></td>
+									<td>Rp. <?= number_format($o['total'], 2, ',', '.') ?></td>
+									<td>
+										<?php if($o['status'] == 'waiting') : ?>
+											<span class="badge badge-primary"><?= $o['status'] ?></span>
+										<?php elseif($o['status'] == 'paid') : ?>
+											<span class="badge badge-warning text-light"><?= $o['status'] ?></span>
+										<?php elseif($o['status'] == 'delivered') : ?>
+											<span class="badge badge-info"><?= $o['status'] ?></span>
+										<?php elseif($o['status'] == 'cancel') : ?>
+											<span class="badge badge-danger"><?= $o['status'] ?></span>
+										<?php endif; ?>
+									</td>
+								</tr>
+							<?php endforeach ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
---
+++ application/views/pages/order/index.php
@@ -0,0 +1,50 @@
+<div style="background-color:#0C2D48;">
+<div class="container p-5">
+<div class="container">
+
+	<div class="row mt-4">
+		<div class="col-10">
+			<h3 style="color : white">List Users Orders</h3>
+		</div>
+	</div>
+
+	<?php $this->load->view('layouts/_alert') ?>
+
+	<div class="row mt-4">
+		<div class="col bg-light p-4">
+			<table class="table table-bordered text-center">
+				<thead>
+					<tr>
+						<th>Invoice</th>
+						<th>Date</th>
+						<th>Total</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach($orders as $o) : ?>
+						<tr>
+							<td><a href="<?= base_url('order/detail/' . $o['id']) ?>"><strong><?= $o['invoice'] ?></strong></a></td>
+							<td><?= $o['date'] ?></td>
+							<td>Rp. <?= number_format($o['total'], 2, ',', '.') ?></td>
+							<td>
+								<?php if($o['status'] == 'waiting') : ?>
+									<span class="badge badge-primary"><?= $o['status'] ?></span>
+								<?php elseif($o['status'] == 'paid') : ?>
+									<span class="badge badge-warning text-light"><?= $o['status'] ?></span>
+								<?php elseif($o['status'] == 'delivered') : ?>
+									<span class="badge badge-info"><?= $o['status'] ?></span>
+								<?php elseif($o['status'] == 'cancel') : ?>
+									<span class="badge badge-danger"><?= $o['status'] ?></span>
+								<?php endif; ?>
+							</td>
+						</tr>
+					<?php endforeach ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+</div>
+</div>
+
